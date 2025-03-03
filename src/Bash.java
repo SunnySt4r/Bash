31fdf822a87fe4ebf93a793c00f3b4a00acfcabf
sunnyst4r <ivan_ivanov_13.02@mail.ru>
2025-03-03 16:16:45 +0300

    first try

--- src/Bash.java
+++ src/Bash.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
 
 public class Bash {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String name = "";
+        while (true) {
+            System.out.printf("|%s> ", name);
+            String command = sc.nextLine();
+            if (command.equals("exit")) {
+                break;
+            }
+            if (command.startsWith("setname ")) {
+                name = command.substring(8);
+                continue;
+            }
+        }
+        sc.close();
     }
 }
