ff444319dc215cbf351e0b85c8fa46d9d26d2e6c
Алтунина Екатерина <166242132+mcfluffybottoms@users.noreply.github.com>
2025-05-15 17:06:51 +0300

    added tests + commented out limits for coverage

--- src/test/java/com/example/command/LsTest.java
+++ src/test/java/com/example/command/LsTest.java
@@ -21,12 +21,62 @@ public class LsTest {
     }
 
     @Test
-    void putArgs_DoesNothing() {
+    void execute_LsWithArguments() {
+        String args[] = {"src/test/testsForFolderLookup"};
+        Ls lsCommand = new Ls(args);
+        ExecutionResult result = lsCommand.execute();
+        assertTrue(result.isSuccess());
+        assertFalse(result.getOutput().isEmpty());
+        assertEquals("a_first.file.txt\n" +
+                "empty_folder\n" +
+                "folder\n" +
+                "visible_file.txt", result.getOutput());
+        assertEquals("", result.getError());
+    }
+
+    @Test
+    void putArgs_FailsWhenIncorrectDataPassed() {
         Ls lsCommand = new Ls();
         lsCommand.putArgs("some input from pipe");
         ExecutionResult result = lsCommand.execute();
+        assertFalse(result.isSuccess());
+        assertEquals("ls: cannot access '" + "some input from pipe" + "': No such file or directory", result.getError());
+    }
+
+    @Test
+    void putArgs_GetIfDirectory() {
+        Ls lsCommand = new Ls();
+        lsCommand.putArgs("src/test/testsForFolderLookup");
+        ExecutionResult result = lsCommand.execute();
         assertTrue(result.isSuccess());
-        assertFalse(result.getOutput().isEmpty()); // putArgs не должен изменять поведение ls
+        assertFalse(result.getOutput().isEmpty());
+        assertEquals("a_first.file.txt\n" +
+                "empty_folder\n" +
+                "folder\n" +
+                "visible_file.txt", result.getOutput());
         assertEquals("", result.getError());
     }
+
+    @Test
+    void putArgs_GetIfNotDirectory() {
+        Ls lsCommand = new Ls();
+        lsCommand.putArgs("src/test/testsForFolderLookup/visible_file.txt");
+        ExecutionResult result = lsCommand.execute();
+        assertTrue(result.isSuccess());
+        assertFalse(result.getOutput().isEmpty());
+        assertEquals("src/test/testsForFolderLookup/visible_file.txt", result.getOutput());
+        assertEquals("", result.getError());
+    }
+
+    @Test
+    void putArgs_GetHome() {
+        Ls lsCommand = new Ls();
+        lsCommand.putArgs("~");
+        ExecutionResult result = lsCommand.execute();
+        assertTrue(result.isSuccess());
+        assertFalse(result.getOutput().isEmpty());
+        assertEquals("", result.getError());
+    }
+
+
 }
