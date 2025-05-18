62592bb9d1a9d3d8092aaff0af1e29e5a1b675dc
Алтунина Екатерина <166242132+mcfluffybottoms@users.noreply.github.com>
2025-05-18 23:38:51 +0300

    fixed mistakes + added tests for multiple args

--- src/test/java/com/example/command/LsTest.java
+++ src/test/java/com/example/command/LsTest.java
@@ -1,6 +1,7 @@
 package com.example.command;
 
 import com.example.utils.ExecutionResult;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -28,7 +29,6 @@ public class LsTest {
         assertTrue(result.isSuccess());
         assertFalse(result.getOutput().isEmpty());
         assertEquals("a_first.file.txt\n" +
-                "empty_folder\n" +
                 "folder\n" +
                 "visible_file.txt", result.getOutput());
         assertEquals("", result.getError());
@@ -51,7 +51,6 @@ public class LsTest {
         assertTrue(result.isSuccess());
         assertFalse(result.getOutput().isEmpty());
         assertEquals("a_first.file.txt\n" +
-                "empty_folder\n" +
                 "folder\n" +
                 "visible_file.txt", result.getOutput());
         assertEquals("", result.getError());
@@ -76,6 +75,104 @@ public class LsTest {
         assertTrue(result.isSuccess());
         assertFalse(result.getOutput().isEmpty());
         assertEquals("", result.getError());
+    }
+
+    @Test
+    void execute_MultipleDirectories() {
+        String args[] = {"src/test/testsForFolderLookup", "src/test/testsForFolderLookup/folder"};
+        Ls lsCommand = new Ls(args);
+        ExecutionResult result = lsCommand.execute();
+        assertTrue(result.isSuccess());
+        assertFalse(result.getOutput().isEmpty());
+        String ans = "src/test/testsForFolderLookup:\n" +
+                "a_first.file.txt\n" +
+                "folder\n" +
+                "visible_file.txt\n" +
+                "\n" +
+                "src/test/testsForFolderLookup/folder:\n" +
+                "a_first_file_inside_folder.txt\n" +
+                "file_inside_folder.txt";
+        assertEquals(ans, result.getOutput());
+        assertEquals("", result.getError());
+    }
+
+    @Test
+    void putArgs_MultipleDirectories() {
+        String args = "src/test/testsForFolderLookup src/test/testsForFolderLookup/folder";
+        Ls lsCommand = new Ls();
+        lsCommand.putArgs(args);
+        ExecutionResult result = lsCommand.execute();
+        assertTrue(result.isSuccess());
+        assertFalse(result.getOutput().isEmpty());
+        String ans = "src/test/testsForFolderLookup:\n" +
+                "a_first.file.txt\n" +
+                "folder\n" +
+                "visible_file.txt\n" +
+                "\n" +
+                "src/test/testsForFolderLookup/folder:\n" +
+                "a_first_file_inside_folder.txt\n" +
+                "file_inside_folder.txt";
+        assertEquals(ans, result.getOutput());
+        assertEquals("", result.getError());
+    }
+
+    @Test
+    void putArgs_MultiplePutArgsCleansAll() {
+        String args[] = {"src/test/testsForFolderLookup", "src/test/testsForFolderLookup/folder"};
+        Ls lsCommand = new Ls();
+        lsCommand.putArgs(args[0]);
+        lsCommand.putArgs(args[1]);
+        ExecutionResult result = lsCommand.execute();
+        assertTrue(result.isSuccess());
+        assertFalse(result.getOutput().isEmpty());
+        String ans =
+                "a_first_file_inside_folder.txt\n" +
+                "file_inside_folder.txt";
+        assertEquals(ans, result.getOutput());
+        assertEquals("", result.getError());
+    }
+
+    @Test
+    void putArgs_MultipleFiles() {
+        String args[] = {"src/test/testsForFolderLookup/a_first.file.txt", "src/test/testsForFolderLookup/visible_file.txt"};
+        Ls lsCommand = new Ls(args);
+        ExecutionResult result = lsCommand.execute();
+        assertTrue(result.isSuccess());
+        assertFalse(result.getOutput().isEmpty());
+        String ans =
+                "src/test/testsForFolderLookup/a_first.file.txt\n" +
+                "src/test/testsForFolderLookup/visible_file.txt";
+        assertEquals(ans, result.getOutput());
+        assertEquals("", result.getError());
+    }
+
+    @Test
+    void putArgs_FilesAndDirectory() {
+        String args[] = {"src/test/testsForFolderLookup/a_first.file.txt", "src/test/testsForFolderLookup/folder"};
+        Ls lsCommand = new Ls(args);
+        ExecutionResult result = lsCommand.execute();
+        assertTrue(result.isSuccess());
+        assertFalse(result.getOutput().isEmpty());
+        String ans =
+                "src/test/testsForFolderLookup/a_first.file.txt\n" +
+                "src/test/testsForFolderLookup/folder:\n" +
+                "a_first_file_inside_folder.txt\n" +
+                "file_inside_folder.txt";
+        assertEquals(ans, result.getOutput());
+        assertEquals("", result.getError());
+    }
+
+    @Test
+    void putArgs_ErrorAndFiles() {
+        String args[] = {"bruh", "src/test/testsForFolderLookup/a_first.file.txt"};
+        Ls lsCommand = new Ls(args);
+        ExecutionResult result = lsCommand.execute();
+        assertFalse(result.isSuccess());
+        assertTrue(result.getOutput().isEmpty());
+        String ans =
+                "ls: cannot access '" + "bruh" + "': No such file or directory\n" +
+                "src/test/testsForFolderLookup/a_first.file.txt";
+        assertEquals(ans, result.getError());
     }
 
 }
